0eb1c68ff62e44e3362065536bf2d735f9e6a033
AbelMP17 <abelmp890@gmail.com>
2023-11-15 14:21:49 +0100

    Pagina Tacon de Oro

---
+++ assets/main.js
@@ -0,0 +1,41 @@
+const menuButton = document.getElementById('menuButton');
+const menuContent = document.getElementById('menuContent');
+
+const menuFilterContent = document.getElementById('menuFilterContent');
+const menuFilter = document.getElementById('menuFilter');
+
+menuFilter.addEventListener('click', () => {
+  if (menuFilter.style.display === 'block' || menuFilterContent.style.display === 'flex') {
+    menuFilterContent.style.display = 'none';
+  } else {
+    menuFilterContent.style.display = 'flex';
+    menuFilterContent.style.flexDirection = 'column';
+    menuFilterContent.style.justifyContent = 'center';
+    menuFilterContent.style.alignItems = 'center';
+    menuFilterContent.style.padding = '10px';
+
+  }
+})
+
+menuButton.addEventListener('click', () => {
+  if (menuButton.style.display === 'block' || menuContent.style.display === 'flex') {
+    menuContent.style.display = 'none';
+  } else {
+    menuContent.style.display = 'flex';
+    menuContent.style.flexDirection = 'column';
+    menuContent.style.justifyContent = 'center';
+    menuContent.style.alignItems = 'center';
+    menuContent.style.padding = '10px';
+
+  }
+});
+
+document.addEventListener('scroll', () => {
+  if (menuContent.style.display === 'flex') {
+    menuContent.style.display = 'none';
+  }
+
+  if(menuFilterContent.style.display === 'flex') {
+    menuFilterContent.style.display = 'none';
+  }
+});
---
+++ readme.md
@@ -0,0 +1,5 @@
+**Ejercicio 1**
+> Lineas del CSS 317 - 333
+
+**Ejercicio 2**
+> Lineas del CSS 267 - 315
